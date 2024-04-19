b073e846fdaec4399a2da17747057a4615eb4571
ashfaqjani916 <ashfaqjani916@gmail.com>
2024-04-19 23:40:24 +0530

    added login and signup page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,5 +1,54 @@
+import { FcGoogle } from 'react-icons/fc'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Input } from '@/components/ui/input'
+
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Link } from 'react-router-dom'
+
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className="flex flex-col border-slate-100 items-center h-[100vh] justify-center">
+      <div>
+        <Avatar style={{ height: '150px', width: '150px' }}>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+
+      <div className="text-[50px] font-custom text-[var(--text-color)]">
+        <h1>9 Hacks</h1>
+      </div>
+
+      <div className="input">
+        <Card style={{ width: '300px', height: '300px' }}>
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <Input type="text" placeholder="Username" />
+          </CardContent>
+          <CardContent>
+            <Input type="password" placeholder="Password" />
+          </CardContent>
+          <CardContent className="flex gap-5 items-center justify-center">
+            <Link className="hover:underline" to="/signup">
+              {' '}
+              SignUp
+            </Link>
+            <Button>Login</Button>
+          </CardContent>
+          <CardContent className=" flex justify-center ">
+            <Button className="bg-transparent h-10 gap-3">
+              <FcGoogle size={30} />
+              Login with Google
+            </Button>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  )
 }
 
 export default Login
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,5 +1,107 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+
+const formSchema = z.object({
+  username: z.string().min(8, {
+    message: 'Username must be at least 8 characters.',
+  }),
+  email: z.string().email({
+    message: 'Invalid email address.',
+  }),
+  password: z.string().min(8, {
+    message: 'Please Input a valid password',
+  }),
+})
+
 const Signup = () => {
-  return <div>Signup</div>
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      email: '',
+      password: '',
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <div>
+      <div className="flex flex-col border-slate-100 items-center h-[100vh] justify-center">
+        <div>
+          <Avatar style={{ height: '150px', width: '150px' }}>
+            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarFallback>CN</AvatarFallback>
+          </Avatar>
+        </div>
+
+        <div className="text-[50px] font-custom text-[var(--text-color)]">
+          <h1>Sign Up</h1>
+        </div>
+
+        <div className="input">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input placeholder="naam" {...field} />
+                    </FormControl>
+                    <FormDescription>This is your public display name.</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="example@example.com" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input placeholder="" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Signup
